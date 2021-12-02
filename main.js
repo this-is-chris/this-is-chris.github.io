54e423328c63776202c32a6e41c34851e3926024
this-is-chris <chrisfranck90@gmail.com>
2021-12-02 22:35:18 +0100

    added currentDate

--- main.js
+++ main.js
@@ -1,26 +1,32 @@
 const calendarContainer = document.querySelector(".container");
 
 const calendarDays = 24;
+const currentDate = new Date();
+const currentDay = currentDate.getDate();
 
-const openDoor = (day, event) => {
+const openDoorFirst = (day, event) => {
     event.target.style.opacity = "0.66";
     event.target.addEventListener("click", openDoorSecond.bind(null, day));
 }
 
 const openDoorSecond = (day, event) => {
-    if (day == 1 || 2) {
+    if (day <= currentDay) {
         event.target.parentNode.style.backgroundImage = `url(./images/tuer-${day}.png)`;
-        //event.target.addEventListener("click", openDoorThird.bind(null, day));
+        if (day == 5 || 24) {
+            event.target.addEventListener("click", openDoorThird.bind(null, day));
+        }
     }
 }
-/*
-const openDoorThird = (day, event) => {
-    if (day == 1) {
+
+const openDoorThird = (day) => {
+    if (day == 5) {
+        // Music Player?
+    } else if (day == 24) {
         let url = `./tueren/tuer-${day}.html`;
         window.open(url, '_blank').focus();
     }
 }
-*/
+
 const createCalendar = () => {
     for(let i = 0; i  < calendarDays; i++) {
         const calendarDoor = document.createElement("div");
@@ -36,6 +42,6 @@ const createCalendar = () => {
 
         day = i + 1;
 
-        calendarDoorText.addEventListener("click", openDoor.bind(null, day));
+        calendarDoorText.addEventListener("click", openDoorFirst.bind(null, day));
     }
 }
